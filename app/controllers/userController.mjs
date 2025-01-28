04a4d46af800915137a87725fa0470fa4e82eeb6
Emir Krasniqi <ph76act@eduvaud.ch>
2025-01-28 11:17:34 +0100

    Implémentation de la view html et css

--- app/controllers/userController.mjs
+++ app/controllers/userController.mjs
@@ -1,11 +1,17 @@
 // Fonction de récupération de tous les utilisateurs
 const getUsers = (req, res) => {
-  res.send("Liste des utilisateurs");
+  // Afficher la liste des utilisateurs dans index.html
+  res.sendFile("public/index.html", { root: "./" });
 };
 
 // Fonction de création d'un utilisateur
 const createUser = (req, res) => {
   res.json({ message: "Utilisateur créé" });
+};
+
+// Fonction de login d'un utilisateur
+const loginUser = (req, res) => {
+  res.json({ message: "Utilisateur connecté" });
 };
 
 // Export des fonctions
